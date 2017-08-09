9f34ee8d48d808e68f4e9b4813ca947a091e9f16
DarkSyz <darksys@sina.com>
2017-08-09 23:53:33 +0800

    add key for menuitems

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ import SearchComponent from './search';
 import AddComponent from './add';
 import { Menu } from 'antd';
 
+class MainMenu extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedKeys: ['search']
+    }
+  }
+
+  render() {
+    return (
+      <Menu className="Header" mode="horizontal" theme="dark"
+        defaultSelectedKeys={this.state.selectedKeys}>
+        <Menu.Item key='search'><Link to='/search'>Search</Link></Menu.Item>
+        <Menu.Item key='add'><Link to='/add'>Add</Link></Menu.Item>
+      </Menu>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -14,18 +33,15 @@ class App extends Component {
         <img src={logo} className="App-logo" alt="logo" />
 
         <Router>
-        <div className="Content">
-          <Menu className="Header" mode="horizontal" theme="dark">
-            <Menu.Item><Link to='/search'>Search</Link></Menu.Item>
-            <Menu.Item><Link to='/add'>Add</Link></Menu.Item>
-          </Menu>
+          <div className="Content">
+            <MainMenu />
 
-          <div className="Body">
-            <Redirect exact from='/' to='/search' />
-            <Route path='/search' component={SearchComponent}/>
-            <Route path='/add' component={AddComponent}/>
+            <div className="Body">
+              <Redirect exact from='/' to='/search' />
+              <Route path='/search' component={SearchComponent} />
+              <Route path='/add' component={AddComponent} />
+            </div>
           </div>
-        </div>
         </Router>
       </div>
     );
